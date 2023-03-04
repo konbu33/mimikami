3cd790a503390c755437684bbd825a042d7bc769
konbu33 <konbu33@gmail.com>
2023-03-04 09:03:21 +0900

    refactor: ファイル名で整列するように、ファイル名を変更

--- packages/text_to_speech/lib/src/slider_section_widget.dart
+++ packages/text_to_speech/lib/src/slider_section_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'pitch_widget.dart';
-import 'rate_widget.dart';
-import 'volume_widget.dart';
+import 'slider_section_pitch_widget.dart';
+import 'slider_section_rate_widget.dart';
+import 'slider_section_volume_widget.dart';
 
 class SliderSectionWidget extends StatelessWidget {
   const SliderSectionWidget({super.key});
