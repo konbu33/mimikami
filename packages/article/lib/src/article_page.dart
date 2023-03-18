eea2ba0342f90b400111a1e072021f776a773173
konbu33 <konbu33@gmail.com>
2023-03-18 13:46:29 +0900

    feat: local_dbへinsertするために用意したボタンが不要になったため、除去

--- packages/article/lib/src/article_page.dart
+++ packages/article/lib/src/article_page.dart
@@ -1,6 +1,3 @@
-import 'package:article/src/article_repository.dart';
-import 'package:article/src/article_state.dart';
-import 'package:common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:receive_share/receive_share.dart';
@@ -37,9 +34,6 @@ class ArticlePage extends StatelessWidget {
 
             // このWidgetで、他アプリからShareしてもらったURLを受け取る。
             ArticlePageParts.receiveShareWidget(),
-
-            // local Db
-            ArticlePageParts.localDbWidget(),
           ],
         );
       }),
@@ -77,39 +71,6 @@ class ArticlePageParts {
   // このWidgetで、他アプリからShareしてもらったURLを受け取る。
   static Widget receiveShareWidget() {
     const widget = ReceiveShareWidget();
-    return widget;
-  }
-
-  // --------------------------------------------------
-  // localDbWidget
-  // --------------------------------------------------
-  static Widget localDbWidget() {
-    final widget = Consumer(builder: (context, ref, child) {
-      // final localDb = ref.watch(localDbDriftProvider);
-      // ref.watch(driftDbProvider);
-      // ref.watch(articleRepositoryProvider);
-      // final articleRepository = ref.watch(articleRepositoryProvider.notifier);
-      final articleRepository = ref.watch(articleRepositoryProvider);
-
-      return ElevatedButton(
-        onPressed: () async {
-          final artcileState = ArticleState.create(
-            id: DateTime.now().millisecondsSinceEpoch.toString(),
-            uriString: "uriString",
-            title: "title",
-            contents: "contents",
-          );
-
-          await articleRepository.addArticle(articleState: artcileState);
-
-          final res = await articleRepository.getAllarticles();
-          logger.d("localDbDrift data : $res");
-
-          // await articleRepository.close();
-        },
-        child: const Text("insert DB"),
-      );
-    });
     return widget;
   }
 }
