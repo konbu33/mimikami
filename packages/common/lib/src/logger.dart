c338289bd0a50723cd837b3079477ba679b66d7b
konbu33 <konbu33@gmail.com>
2023-03-29 10:15:56 +0900

    feat: LoggerのLevelを変更する関数を実装

--- packages/common/lib/src/logger.dart
+++ packages/common/lib/src/logger.dart
@@ -13,3 +13,24 @@ LogPrinter logPrinter = PrettyPrinter(
 Logger logger = Logger(
   printer: logPrinter,
 );
+
+void changeLoggerLevel(String level) {
+  Level newLevel = Level.debug;
+
+  switch (level) {
+    case 'debug':
+      newLevel = Level.debug;
+      break;
+
+    case 'info':
+      newLevel = Level.info;
+      break;
+
+    default:
+  }
+
+  logger = Logger(
+    printer: logPrinter,
+    level: newLevel,
+  );
+}
